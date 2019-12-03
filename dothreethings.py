f15f4311c8148df51673416c5946ef667ceec552
duch94 <vtduch@gmail.com>
2019-12-03 10:13:42 +0300

    added solution for 4th task

--- dothreethings.py
+++ dothreethings.py
@@ -1,12 +1,24 @@
+from time import sleep
+
+
 class Doer:
+    _first_executed = False
+    _second_executed = False
+
     def first(self, action):
         # Следующую строчку не убирать
         action()
+        self._first_executed = True
 
     def second(self, action):
+        while not self._first_executed:
+            sleep(0.1)
         # Следующую строчку не убирать
         action()
+        self._second_executed = True
 
     def third(self, action):
+        while not self._second_executed:
+            sleep(0.1)
         # Следующую строчку не убирать
         action()
---
+++ test_task4.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+import threading
+
+from dothreethings import Doer
+
+
+class TestDoThreeThings(TestCase):
+    @staticmethod
+    def some_action_1():
+        print("some action 1")
+
+    @staticmethod
+    def some_action_2():
+        print("some action 2")
+
+    @staticmethod
+    def some_action_3():
+        print("some action 3")
+
+    def test_pipeline(self):
+        d = Doer()
+        t1 = threading.Thread(target=d.third, args=[self.some_action_3])
+        t2 = threading.Thread(target=d.second, args=[self.some_action_2])
+        t3 = threading.Thread(target=d.first, args=[self.some_action_1])
+
+        for t in [t1, t2, t3]:  # type: Thread
+            t.start()
