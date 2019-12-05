746bf63c172e3093c198f8a959550898a5d15fd4
duch94 <vtduch@gmail.com>
2019-12-06 00:48:31 +0300

    added docstrings for 2 and 3 tasks

--- task2.py
+++ task2.py
@@ -2,20 +2,47 @@ import time
 
 
 def func_timeit_decorator(func):
+    """
+    Функция-декоратор для подсчета времени работы оборачиваемой функции.
+    :param func: оборачиваемая функция
+    :return: обёрнутая функция
+    """
+
     def wrapper_func(*args, **kwargs):
+        """
+        Функция, реализующая подсчет времени работы оборачиваемой функции, вызываемой с помощью замыкания.
+        :param args: передаваемые в оборачиваемую функцию позиционные аргументы
+        :param kwargs: передаваемые в оборачиваемую функцию именованные аргументы
+        :return: None
+        """
         start_time = time.time()
         func(*args, **kwargs)
         end_time = time.time()
         print(end_time - start_time)
+
     return wrapper_func
 
 
 class ClassTimeitDecorator:
+    """
+    Класс-декоратор для подсчета времени работы оборачиваемой функции
+    """
 
     def __init__(self, func):
+        """
+        Метод-конструктор класса, который принимает в себя оборачиваемую функцию.
+        :param func: оборачиваемая функция
+        :return: self
+        """
         self.function = func
 
     def __call__(self, *args, **kwargs):
+        """
+        Магический метод, фактически перегружающий оператор (). Реализует подсчет времени работы оборачиваемой функции.
+        :param args: передаваемые в оборачиваемую функцию позиционные аргументы
+        :param kwargs: передаваемые в оборачиваемую функцию именованные аргументы
+        :return: None
+        """
         start_time = time.time()
         self.function(*args, **kwargs)
         end_time = time.time()
--- task3.py
+++ task3.py
@@ -1,5 +1,14 @@
 class LetsIterate:
+    """
+    Класс, реализующий итератор передаваемого объекта
+    """
     def __init__(self, iterable_obj, return_odd_elements=True):
+        """
+        Конструктор, в который передается итерируемый объект и
+        флаг для указания какие по четности элементы использовать.
+        :param iterable_obj: итерируемый объект
+        :param return_odd_elements: True, если нужно возвращать нечетные элементы
+        """
         self.iterable_obj = iterable_obj
         assert(isinstance(return_odd_elements, bool))
         if return_odd_elements:
@@ -8,9 +17,17 @@ class LetsIterate:
             self.i = 0
 
     def __iter__(self):
+        """
+        Магический метод, необходимый для того, чтобы класс считался итератором.
+        :return: self
+        """
         return self
 
     def __next__(self):
+        """
+        Магический метод, возвращающий следующий четный или нечетный элемент итерируемого объекта.
+        :return: следующий четный или нечетный элемент итерируемого объекта
+        """
         try:
             res = self.iterable_obj[self.i]
         except IndexError:
